6f52a5c65fc42546fb818338e1efd13d0e2c4cae
gavinfitch <gavin.fitch@gmail.com>
2021-11-07 13:09:55 -0800

    AWS working

--- frontend/src/components/UploadPhotoForm/index.js
+++ frontend/src/components/UploadPhotoForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useHistory, NavLink } from "react-router-dom";
+import S3 from 'react-aws-s3';
 import * as sessionActions from "../../store/session";
 import './UploadPhotoForm.css';
 import Logo from '../Logo';
@@ -11,19 +12,46 @@ function UploadPhotoForm() {
     const sessionUser = useSelector((state) => state.session.user);
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
-    const [photo, setPhoto] = useState(null);
+    const [photo, setPhoto] = useState();
     // const [album, setAlbum] = useState(null);
     const [errors, setErrors] = useState([]);
 
     if (!sessionUser) return <Redirect to="/" />;
 
+    const config = {
+        bucketName: 'pixelphotostorage',
+        // dirName: 'media', /* optional */
+        region: 'us-west-2',
+        accessKeyId: 'AKIAQ5HCEL66DJMSJ66K',
+        secretAccessKey: 'imq9J1MpJbvhLqSvxyG0OTf+tS6OWllAl3np6cly',
+        // s3Url: 'https:/your-custom-s3-url.com/', /* optional */
+    }
+
+    const ReactS3Client = new S3(config);
+
     const redirectHome = () => {
         history.push("/")
     };
 
-    const handleSubmit = (e) => {
+    const fileSelectedHandler = event => {
+        console.log(event.target.files[0])
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log(photo);
+        // console.log(title, description, photo)
+
+        // const testFetch = await fetch('https://pixelphotostorage.s3.us-west-2.amazonaws.com/4c7tobjdCxZTmT4vc6YNVJ.jpeg')
+        // console.log(testFetch)
+        let amazonPhoto;
+        await ReactS3Client
+            .uploadFile(photo, title)
+            .then(data => amazonPhoto = data)
+            .catch(err => console.error(err))
+
+        console.log("this is amazon photo ", amazonPhoto.location)
+
+
         // if (password === confirmPassword) {
         //     setErrors([]);
         //     return dispatch(sessionActions.thunk_signup({ firstName, lastName, username, email, password }))
@@ -77,8 +105,8 @@ function UploadPhotoForm() {
                         <input
                             className="form-field"
                             type="file"
-                            value={photo}
-                            onChange={(e) => setPhoto(e.target.value)}
+                            // value={photo.name}
+                            onChange={(e) => setPhoto(e.target.files[0])}
                         // required
                         />
                         <button className="form-button" type="submit">Upload</button>
